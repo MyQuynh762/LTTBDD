6822bcf0977f3a7645b47ab8c4b9d91342837104
MyQuynh762 <144238399+MyQuynh762@users.noreply.github.com>
2023-10-18 16:49:36 +0700

    tuan03

---
+++ tuan03/MyReact/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+const App =()=>{
+  const [text,onChangeText]= React.useState('Useless Text');
+
+return (
+
+  <Text>Hello</Text>
+    
+ 
+  
+);
+};
+
+  
+
+const styles = StyleSheet.create({
+   
+ 
+});
+
+export default App;
